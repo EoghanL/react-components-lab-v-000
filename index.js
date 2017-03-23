d5ff4b361ef38a481b7c7ae9ddbbd60c8ec3c2d0
github username <github email address>
2017-03-23 12:48:34 -0400

    done, fix tests, functional rendering

--- index.js
+++ index.js
@@ -5,6 +5,48 @@ const BUTCHER_PRODUCTS = [
   'Ribeye'
 ];
 
+const pCreator = function(text){
+  return React.createElement('p', {}, `${text}`)
+}
+
+const liGenerator = function(array){
+  return array.map((element) => React.createElement('li', {}, element))
+}
+
+const innerStrong = React.createElement('strong', {}, 'right in front of you.')
+
+const OlderCoaster = React.createClass({
+  render() {
+    return React.createElement('div', { className: 'oldercoaster'}, [
+      pCreator('Two grannies having the time of their life!'),
+      pCreator('Passengers:'),
+      React.createElement('ul', {}, [
+        liGenerator(['Agnes', 'Muriel'])
+      ])
+    ])
+  }
+});
+
+class InFrontOfYou extends React.Component{
+  render(){
+    return React.createElement('div', {}, [
+      pCreator("You shouldn't look too far."),
+      React.createElement('p', {}, ["Sometimes, the solution is ", innerStrong])
+    ])
+  }
+}
+
+class ButcherShop extends React.Component{
+  render(){
+    return React.createElement('div', { className: 'butcher-shop' }, [
+      pCreator('Hello! We have the following products for sale today:'),
+      React.createElement('ul', {}, liGenerator(BUTCHER_PRODUCTS) )
+    ])
+  }
+}
+
+
+
 ReactDOM.render(
   React.createElement('div', {}, [
     React.createElement(OlderCoaster),
@@ -13,3 +55,16 @@ ReactDOM.render(
   ]),
   document.getElementById('main')
 );
+
+// const OlderCoaster = React.createClass({ ALSO BROKE TEST NUMBER 2
+//   render() {
+//     return React.createElement('div', { className: 'oldercoaster' }, [
+//       React.createElement('p', {}, 'Two grannies having the time of their life!'),
+//       React.createElement('p', {}, 'Passengers:'),
+//       React.createElement('ul', {}, [
+//         React.createElement('li', {}, 'Agnes'),
+//         React.createElement('li', {}, 'Muriel'),
+//       ])
+//     ]);
+//   }
+// });
